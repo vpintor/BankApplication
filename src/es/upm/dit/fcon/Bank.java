77713374377f07c32fd5c190dcf1ed5bd8d899ab
v.pintor <v.pintor@alumnos.upm.es>
2019-01-17 20:07:53 +0100

    Scanner funcionando

--- src/es/upm/dit/fcon/Bank.java
+++ src/es/upm/dit/fcon/Bank.java
@@ -74,67 +74,85 @@ public class Bank {
 		}
 		return new Client(accNumber, name, balance);
 	}
+	
+	public String readAll() {
+		String aux = new String();
 
+		for (HashMap.Entry <Long, Client>  entry : clients.entrySet()) {
+			aux = aux + entry.getValue().toString() + "\n";
+		}
+		return aux;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Bank bank = new Bank();
 		Scanner sc = new Scanner(System.in);
+		boolean salir = false;
 		Long accNumber   = (long) 0;
 		int balance     = 0;
 		Client client   = null;
 		int     menuKey = 0;
-		System. out .println(">>> Enter operation client.: 1) Create. 2) Read. 3) Update. 4) Delete. 5) BankDB. 6) Exit");
-		if (sc.hasNextInt()) {
-			menuKey = sc.nextInt();
-		} else {
-			sc.next();
-			System.out.println("The provised text provided is not an integer");
-		}
-		
-		switch (menuKey) {
-		case 1: // Create client
-			bank.createClient(bank.readScanner(sc));
-			break;
-		case 2: // Read client
-			System. out .print(">>> Enter account number (int) = ");
-			if (sc.hasNextInt()) {
-				accNumber = (long) sc.nextInt();
-				client = bank.readClient(accNumber);
-				System.out.println(client);
-			} else {
-				System.out.println("The provised text provided is not an integer");
-				sc.next();
-			}
-			break;
-		case 3: // Update client
-			
-			bank.updateClient(bank.readScanner(sc));
-			break;
-		case 4: // Delete client
-			System. out .print(">>> Enter account number (int) = ");
-			if (sc.hasNextInt()) {
-				accNumber = (long) sc.nextInt();
-				boolean status = bank.deleteClient(accNumber);
-				if (status) {
-					System.out.println("El cliente " +bank.getClients().get(accNumber).getName() + " se ha borrado correctamente.");
+		while(!salir) {
+			try {
+				System. out .println(">>> Enter operation client.: 1) Create. 2) Read. 3) Update. 4) Delete. 5) ReadAll. 6) Exit");
+				if (sc.hasNextInt()) {
+					menuKey = sc.nextInt();
+				} else {
+					sc.next();
+					System.out.println("The provised text provided is not an integer");
 				}
-				else {
-					System.out.println("Se ha producido un error. El cliente con id " + accNumber + " no existe.");
+				
+				switch (menuKey) {
+				case 1: // Create client
+					bank.createClient(bank.readScanner(sc));
+					break;
+				case 2: // Read client
+					System. out .print(">>> Enter account number (int) = ");
+					if (sc.hasNextInt()) {
+						accNumber = (long) sc.nextInt();
+						client = bank.readClient(accNumber);
+						System.out.println(client);
+					} else {
+						System.out.println("The provised text provided is not an integer");
+						sc.next();
+					}
+					break;
+				case 3: // Update client
+					
+					bank.updateClient(bank.readScanner(sc));
+					break;
+				case 4: // Delete client
+					System. out .print(">>> Enter account number (int) = ");
+					if (sc.hasNextInt()) {
+						accNumber = (long) sc.nextInt();
+						String name = bank.getClients().get(accNumber).getName();
+						boolean status = bank.deleteClient(accNumber);
+						if (status) {
+							System.out.println("El cliente " +name+ " se ha borrado correctamente.");
+						}
+						else {
+							System.out.println("Se ha producido un error. El cliente con id " + accNumber + " no existe.");
+						}
+					} else {
+						System.out.println("The provised text provided is not an integer");
+						sc.next();
+					}
+					break;
+				case 5:
+					String aux = bank.readAll();
+					System.out.println(aux);
+					break;
+				case 6:
+					salir = true;	
+					//bank.close();
+				default:
+					break;
 				}
-			} else {
-				System.out.println("The provised text provided is not an integer");
-				sc.next();
 			}
-			break;
-		case 5:
-			String aux = bank.toString();
-			System.out.println(bank.toString());
-			break;
-		case 6:
-			boolean exit = true;	
-			//bank.close();
-		default:
-			break;
+			catch (Exception e) {
+				System.out.println("Exception at Main. Error read data");
+			}
 		}
 		sc.close();
 	}
