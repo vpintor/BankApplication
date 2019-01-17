90e4d527d58bb23c2963792e8339337bbde9a65f
v.pintor <v.pintor@alumnos.upm.es>
2019-01-17 18:50:24 +0100

    SCANNER EN PROCESO

---
+++ src/es/upm/dit/fcon/Bank.java
@@ -0,0 +1,141 @@
+package es.upm.dit.fcon;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Bank {
+
+	private HashMap <Long, Client> clients; 
+	
+	public Bank () {
+		this.clients = new HashMap <Long, Client>();
+	}
+	private boolean createClient(Client client) {
+		if (clients.containsKey(client.getAccountNumber())) {
+			return false;
+		} else {
+			clients.put(client.getAccountNumber(), client);
+			return true;
+		}
+	}
+	
+	private boolean deleteClient(Long id) {
+		if (clients.containsKey(id)) {
+			clients.remove(id);
+			return true;
+		} else {
+			return false;
+		}	
+	}
+	private HashMap<Long, Client> getClients() {
+		return clients;
+	}
+	private boolean updateClient(Client client) {
+		if (clients.containsKey(client.getAccountNumber())) {
+			clients.put(client.getAccountNumber(), client);
+			return true;
+		} else {
+			return false;
+		}	
+	}
+	
+	private Client readClient(Long id) {
+		if (clients.containsKey(id)) {
+			return clients.get(id);
+		} else {
+			return null;
+		}	
+	}
+	
+	public Client readScanner(Scanner sc) {
+		Long accNumber = (long) 0;
+		String name   = null;
+		int balance   = 0;
+		
+		System. out .print(">>> Enter account number (long) = ");
+		if (sc.hasNextInt()) {
+			accNumber = (long)(sc.nextInt());
+		} else {
+			System.out.println("The provised text provided is not an integer");
+			sc.next();
+			return null;
+		}
+
+		System. out .print(">>> Enter name (String) = ");
+		name = sc.next();
+
+		System. out .print(">>> Enter balance (int) = ");
+		if (sc.hasNextInt()) {
+			balance = sc.nextInt();
+		} else {
+			System.out.println("The provised text provided is not an integer");
+			sc.next();
+			return null;
+		}
+		return new Client(accNumber, name, balance);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank bank = new Bank();
+		Scanner sc = new Scanner(System.in);
+		Long accNumber   = (long) 0;
+		int balance     = 0;
+		Client client   = null;
+		int     menuKey = 0;
+		System. out .println(">>> Enter operation client.: 1) Create. 2) Read. 3) Update. 4) Delete. 5) BankDB. 6) Exit");
+		if (sc.hasNextInt()) {
+			menuKey = sc.nextInt();
+		} else {
+			sc.next();
+			System.out.println("The provised text provided is not an integer");
+		}
+		
+		switch (menuKey) {
+		case 1: // Create client
+			bank.createClient(bank.readScanner(sc));
+			break;
+		case 2: // Read client
+			System. out .print(">>> Enter account number (int) = ");
+			if (sc.hasNextInt()) {
+				accNumber = (long) sc.nextInt();
+				client = bank.readClient(accNumber);
+				System.out.println(client);
+			} else {
+				System.out.println("The provised text provided is not an integer");
+				sc.next();
+			}
+			break;
+		case 3: // Update client
+			
+			bank.updateClient(bank.readScanner(sc));
+			break;
+		case 4: // Delete client
+			System. out .print(">>> Enter account number (int) = ");
+			if (sc.hasNextInt()) {
+				accNumber = (long) sc.nextInt();
+				boolean status = bank.deleteClient(accNumber);
+				if (status) {
+					System.out.println("El cliente " +bank.getClients().get(accNumber).getName() + " se ha borrado correctamente.");
+				}
+				else {
+					System.out.println("Se ha producido un error. El cliente con id " + accNumber + " no existe.");
+				}
+			} else {
+				System.out.println("The provised text provided is not an integer");
+				sc.next();
+			}
+			break;
+		case 5:
+			String aux = bank.toString();
+			System.out.println(bank.toString());
+			break;
+		case 6:
+			boolean exit = true;	
+			//bank.close();
+		default:
+			break;
+		}
+		sc.close();
+	}
+}
--- src/es/upm/dit/fcon/Client.java
+++ src/es/upm/dit/fcon/Client.java
@@ -1,5 +1,47 @@
 package es.upm.dit.fcon;
 
-public class Client {
+import java.io.Serializable;
 
+public class Client implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long accountNumber;
+	private String name;
+	private int balance;
+	
+	public Client (Long accountNumber, String name, int balance) {
+		this.accountNumber = accountNumber;
+		this.name          = name;
+		this.balance       = balance;
+	}
+
+	public Long getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(Long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + accountNumber + ", " + name + ", " + balance + "]";
+	}
 }
